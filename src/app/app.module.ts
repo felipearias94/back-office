c80f9829e1025c3911f5eacd9bf2e91943634d58
Felipe Arias <felari94@gmail.com>
2023-06-20 21:56:30 -0300

    main components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainWrapperComponent } from './components/main-wrapper/main-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent
   ],
   imports: [
     BrowserModule
