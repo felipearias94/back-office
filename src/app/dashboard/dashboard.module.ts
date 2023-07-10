76c91aad56171bc642d54359a4fe05aaf7c57a1a
Felipe Arias <felari94@gmail.com>
2023-07-10 11:57:45 -0300

    home.component added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HomeModule],
   exports: [DashboardComponent],
 })
 export class DashboardModule {}
