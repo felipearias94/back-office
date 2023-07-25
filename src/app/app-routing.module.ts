4a2f57bb7f339a670561ed55551d7103684c657e
Felipe Arias <felari94@gmail.com>
2023-07-25 14:35:31 -0300

    auth module WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './dashboard/pages/home/home.component';
 import { UsersComponent } from './dashboard/pages/users/users.component';
 import { UserDetailsComponent } from './dashboard/pages/users/user-details/user-details.component';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { RegisterComponent } from './auth/pages/register/register.component';
 
 const routes: Routes = [
   {
@@ -37,10 +39,20 @@ const routes: Routes = [
   {
     path: 'auth',
     component: AuthComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+    ],
   },
   {
     path: '**',
-    redirectTo: '/auth',
+    redirectTo: '/auth/login',
   },
 ];
 
