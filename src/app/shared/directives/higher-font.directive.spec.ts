24ae3d30037ae7dc7b6ae81b65d59eb65f945ab9
Felipe Arias <felari94@gmail.com>
2023-08-08 22:16:31 -0300

    removed unnecesary spec files

--- src/app/shared/directives/higher-font.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HigherFontDirective } from './higher-font.directive';
-
-describe('HigherFontDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HigherFontDirective();
-    expect(directive).toBeTruthy();
-  });
-});
