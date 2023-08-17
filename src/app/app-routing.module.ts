815337f861ab9f892c2a8ce1abea7e8086c6795e
Felipe Arias <felari94@gmail.com>
2023-08-17 16:28:41 -0300

    unused imports removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthComponent } from './auth/auth.component';
-import { HomeComponent } from './dashboard/pages/home/home.component';
-import { UsersComponent } from './dashboard/pages/users/users.component';
-import { UserDetailsComponent } from './dashboard/pages/users/user-details/user-details.component';
-import { LoginComponent } from './auth/pages/login/login.component';
-import { RegisterComponent } from './auth/pages/register/register.component';
-import { StudentsComponent } from './dashboard/pages/students/students.component';
-import { CoursesComponent } from './dashboard/pages/courses/courses.component';
-import { ClassesComponent } from './dashboard/pages/classes/classes.component';
 import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
